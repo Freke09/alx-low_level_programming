7187c62657c65b93eca79ad6a91e31ab5fd8ba15
Freke09 <ndifrekeh@gmail.com>
2023-08-13 17:46:56 -0700

    A program to prints all the single digits of base ten using int and putchar only

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * A program that prints all singler digits of base ten
+ * using int and putchar only
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num);
+		num++;
+	}
+
+	putchar('\n');
+
+	return (0):
+}
