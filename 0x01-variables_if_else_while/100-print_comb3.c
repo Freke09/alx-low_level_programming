bfd283a722ad14a84fc7e7208d4f2947105efcf6
Freke09 <ndifrekeh@gmail.com>
2023-08-14 16:31:43 -0700

    A program that prints all possible combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,7 +15,6 @@ int main(void)
 	for (num1 = '0'; num1 <= '8'; num1++)
 	{
 		for (num2 = '0'; num2 <= '9'; num2++)
-		
 		{
 			if (num1 < num2)
 			{
@@ -32,7 +31,7 @@ int main(void)
 	}
 	
 	putchar('\n');
-	
+
 	return (0);
 
 }
