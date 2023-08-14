a9123211300f6b2293735f9a1d91d3651612e63a
Freke09 <ndifrekeh@gmail.com>
2023-08-13 17:25:11 -0700

    A program that prints all the single digit  numbers in base ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * A program that prints all the single numbers in base ten
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number = '0';
+
+	while (number <= '9')
+	{
+		printf("%d", number);
+		number++;
+	}
+
+	printf('\n');
+
+	return (0);
+
+}
