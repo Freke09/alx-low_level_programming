8b05090d027e9ce227df6a2ef44d913eff2a0e37
Freke09 <ndifrekeh@gmail.com>
2023-08-15 12:55:08 -0700

    A program that prints string to the standard ouptut

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* A program to print _putchar to standard output
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
