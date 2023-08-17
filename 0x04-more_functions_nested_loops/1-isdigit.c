6518ead2bcda4ce36cbdd024cae78b861f8cffee
Freke09 <ndifrekeh@gmail.com>
2023-08-17 16:05:55 -0700

    A function that checks if a character is a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _isdigit - Checks if a character is a digit
+* _isdigit - Checks if a character is a digit between (0 through 9)
 * @c: the character being checked
 * Return: 1 if it is a digit, 0 otherwis 
 */
