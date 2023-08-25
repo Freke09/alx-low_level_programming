d0c912b4ab5fb3c3a8311fae6f7ec1fc95ab01c4
Freke09 <ndifrekeh@gmail.com>
2023-08-25 04:38:06 -0700

    A function to compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strcmp - A function that compares two strings
+* @s1: First string array
+* @s2: Second string array
+* Return: Always 0
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0, i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			a = 1;
+			break;
+		}
+	}
+
+	if (a = = 1)
+	{
+		_putchar('0');
+	}
+
+}
---
+++ 0x06-pointers_arrays_strings/strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+* _strncat -A function that concatenates two string
+* @dest: Destination string array
+* @src: Source string array
+* @n: The n variabe
+* Return: A prointer 
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	
