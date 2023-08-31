abb0c86867b25177ef5b4f155343ae127d682494
Freke09 <ndifrekeh@gmail.com>
2023-08-31 00:46:35 -0700

    A function that prints a string followed by new line

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Standard library */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+
+/* User(My) defined functions */
+
 void _puts_recursion(char *s);
 
 #endif
