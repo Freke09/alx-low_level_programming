af2068764c4cda9966b36dd98a143f60ddbd0952
Freke09 <ndifrekeh@gmail.com>
2023-09-12 07:20:50 -0700

    Pushed the update so I can have all the files in other workspace

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+* int_dog - function prototype
+* @d: struct dog variable
+* @name: name
+* @age: age
+* @owner: owner's name
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
