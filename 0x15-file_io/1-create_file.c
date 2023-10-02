99abbd752a917c5dc9697125b92f9d60678acb7c
Ndifreke Ndue <115492189+Freke09@users.noreply.github.com>
2023-10-02 17:19:14 +0000

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		written = write(file1, text_content, strlen(text_content));
 		if (written == -1)
