b0f730b7c2711721ca9c2e1f97b3c21f662f6016
Ndifreke Ndue <115492189+Freke09@users.noreply.github.com>
2023-10-02 16:56:30 +0000

    A function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* create_file - Creates a file
+* @filename: The name of file to be created
+* @text_content: The string
+* Return: 1 on success and -1 on failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int file1;
+	ssize_t written;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	file1 = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (file1 == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		written = write(file1, text_content, strlen(text_content));
+		if (written == -1)
+		{
+			close(file1);
+			return (-1);
+		}
+	}
+
+	close(file1);
+
+	return (1);
+}
